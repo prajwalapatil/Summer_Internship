0c1130ba529e52904ae52e894bc851222f64f6fc
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-05-30 03:47:57 -0700

    Task-5

---
+++ day-2/Integration.php
@@ -0,0 +1,14 @@
+<?php
+//DATE : 26/5/2021 TASK-5 Integration Of HTML & PHP
+
+echo "Welcome To PHP";
+echo "<h1>MY NAME IS PRAJWALA PATIL...</h1>"
+?>
+<html>
+<body bgcolor="yellow">
+Welcome To HTML
+  <?php
+  echo "<br><br>I AM PHP Inside HTML";
+  ?>
+</body>
+</html>
