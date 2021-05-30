6ef9e7d8d59797f8fe816a499dc80fb49f6de616
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-05-30 06:33:35 -0700

    Task-4

---
+++ day-3/Do_While&WhileLoop.php
@@ -0,0 +1,22 @@
+<!--DATE : 27/5/2021 TASK-4 Do..While AND While Loop Demo-->
+<html>
+
+<h3>Do..While Loop Demo</h3>
+<?php
+$a=0;
+do{
+echo $a;
+$a++;
+}while($a<=5);
+?>
+</br>
+<h3>While Loop Demo</h3>
+<?php
+$i=0;
+while($i<=10)
+{
+echo $i;
+$i++;	
+}
+?>
+</html>
