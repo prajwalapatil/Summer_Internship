18ea9cd4eeaf8ab95e6e7877cd51ca871eaf5621
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-05-30 03:45:59 -0700

    Task-3

---
+++ day-2/If-elseDemo.php
@@ -0,0 +1,13 @@
+<?php
+//DATE : 26/5/2021 TASK-3 If else Demo
+$a = 12;
+$b = 25;
+
+if($a < $b) {
+	echo "Value of A is Small";
+} else if($b < $a) {
+	echo "Value of B is Small";
+}  else {
+	echo "Values of A and B are Same!";
+}
+?>
