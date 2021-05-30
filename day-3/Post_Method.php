45d3e91cb2103a0c55aee11f37803d1be55ac27b
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-05-30 06:08:32 -0700

    Task-2

---
+++ day-3/Post_Method.php
@@ -0,0 +1,25 @@
+<!-- //DATE : 27/5/2021 TASK-2 POST Method-->
+<html>
+<head>
+	<title>Post Method</title>
+</head>
+<body>
+<h3>Form With Post Method: </h3>
+	<form action="process.php" method="POST">
+		<table>
+			<tr>
+				<td>Name : </td>
+				<td><input type="text" name="txt1" /></td>
+			</tr>
+			<tr>
+				<td>Password : </td>
+				<td><input type="Number" name="txt2" /></td>
+			</tr>
+			<tr>
+				<td><input type="submit" /></td>
+				<td><input type="reset" /></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
