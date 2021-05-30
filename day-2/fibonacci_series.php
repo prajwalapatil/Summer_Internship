c2820b0d0e8ff71e5f6d8c9965c50ed4f32252e6
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-05-30 04:00:16 -0700

    Task-6

---
+++ day-2/fibonacci_series.php
@@ -0,0 +1,32 @@
+<?php
+//DATE : 26/5/2021 TASK-6 Fibonacci Series
+?>
+<html>
+<title>Factorial Of Number</title>
+<body bgcolor="pink">
+<form action="#" method="post">
+<label>Enter Number</label>
+<input type="number" name="num" value="num"> 
+<br><br>
+<input type="submit">
+<br><br>
+</html>
+<?php 
+
+function Fibonacci($number){ 
+	if ($number == 0) {
+		return 0;	 
+	} else if ($number == 1) {
+		return 1;	 
+	}
+	else
+		return (Fibonacci($number-1) + Fibonacci($number-2)); 
+} 
+
+$number = $_POST['num']; 
+
+echo "fibonacci series upto $number number : <br>";
+for ($counter = 0; $counter < $number; $counter++) { 
+	echo Fibonacci($counter),', '; 
+} 
+?> 
