9c887c3d8001f51ca273bd56f8f8c288010ef87d
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-05-30 06:00:37 -0700

    Task-1

---
+++ day-3/Get_Method.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- //DATE : 27/5/2021 TASK-1 GET Method-->
+
+<html>
+<head>
+	<title>Get Method</title>
+</head>
+<body>
+
+	<h3>Form With Get Method : </h3>
+	<form method="GET">
+		<table>
+			<tr>
+				<td>Name : </td>
+				<td><input type="text" name="txt1" required /></td>
+			</tr>
+			<tr>
+				<td>Password: </td>
+				<td><input type="Number" name="txt2" /></td>
+			</tr>
+			<tr>
+				<td><input type="submit" /></td>
+				<td><input type="reset" /></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
+	
