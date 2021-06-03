c2a238ae4c8c54512ba5fbecc78a33da19ab541a
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-06-03 04:11:41 -0700

    Task-2

---
+++ day-5/Numerical_Array.php
@@ -0,0 +1,45 @@
+<!--DATE : 29/5/2021 TASK-1 Student Print Array DEMO-->
+<?php
+//numerical array
+//method 1
+$a[0]=10;
+$a[1]=20;
+$a[2]=20.50;
+$a[3]="c";
+$a[4]="10";
+
+//method 2 dynamic index
+$a[]=10;
+$a[]=20;
+$a[]=20.50;
+$a[]="c";
+$a[]="10";
+
+//method3
+//always use this method to create an array
+
+$a = array(10,20,30,"c","c++",10.50);
+
+//Print array values
+
+echo $a[3];
+
+
+//print whole array
+
+for ($i=0; $i < count($a) ; $i++) { 
+	echo $a[$i]." <br/>";
+}
+
+$sum = array_sum($a);
+echo "$sum  <br/>";
+
+//3 inbuilt functions to debug an array
+echo "<pre>";
+print_r($a);
+echo "<pre>";
+echo "<pre>";
+var_dump($a);
+echo "<pre>";
+
+?>
