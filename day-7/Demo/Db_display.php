c97e0ddcf24d646f51893b3565f4d939f02d58b2
prajwalapatil <85019373+prajwalapatil@users.noreply.github.com>
2021-06-11 23:06:14 -0700

    Demo

---
+++ day-7/Demo/Db_display.php
@@ -0,0 +1,34 @@
+<!--DATE : 2/6/2021 TASK-1 Display Database DEMO-->
+<?php 
+
+	$host = "localhost";
+	$username = "root";
+	$passwd = "";
+	$dbname = "db_internship";
+
+	$connection = mysqli_connect($host, $username, $passwd, $dbname);
+
+	$q = mysqli_query($connection,
+		"select * from tbl_user where is_delete = 0") or die("Error". mysqli_error($connection));
+
+	echo "<table border=1>";
+	echo "<tr>";
+	echo "<td>Id</td>";
+	echo "<td>Name</td>";
+	echo "<td>Gender</td>";
+	echo "<td>Mobile</td>";
+	echo "<td>Action</td>";
+	echo "</tr>";
+	
+	while ($row = mysqli_fetch_array($q)) {
+		echo "<tr>";
+		echo "<td>{$row['user_id']}</td>";
+		echo "<td>{$row['user_name']}</td>";
+		echo "<td>{$row['user_gender']}</td>";
+		echo "<td>{$row['user_mobile']}</td>";
+		echo "<td><a href='db_delete.php?deleteid={$row['user_id']}'> Delete </a></td>";
+		echo "</tr>";
+	}
+	echo "<tr> <td> <a href='Db_add.php'> Add Record </a> </td> </tr>";
+	echo "</table>";
+?>
